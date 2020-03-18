cf6430e556c3558be38f47c05c7fd42c577be3c7
Injeong Choi <choinj97@gmail.com>
2020-03-19 00:06:16 +0900

    Initial Commit

---
+++ MyfirstiOSApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyfirstiOSApp
+//
+//  Created by 최인정 on 19/03/2020.
+//  Copyright © 2020 indoni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
